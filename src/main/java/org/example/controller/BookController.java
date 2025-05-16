4512c12d1518a19b14341574465a17eb3df9fe58
Kate Kraska <kate2002.kk42@gmail.com>
2025-05-16 17:15:07 +0300

    fixed mentor code review comments.

--- src/main/java/org/example/controller/BookController.java
+++ src/main/java/org/example/controller/BookController.java
@@ -2,6 +2,7 @@ package org.example.controller;
 
 import org.example.dto.BookDto;
 import org.example.dto.CreateBookRequestDto;
+import org.example.model.Book;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.example.service.BookService;
 import lombok.RequiredArgsConstructor;
+import java.util.List;
 
 @RestController
 @RequestMapping("/books")
@@ -17,6 +19,16 @@ import lombok.RequiredArgsConstructor;
 public class BookController {
 
     private final BookService bookService;
+
+    @GetMapping
+    public List<BookDto> findAll() {
+        return bookService.findAll();
+    }
+
+    @GetMapping
+    public Book save(Book book) {
+        return bookService.save(book);
+    }
 
     @GetMapping("/{id}")
     public BookDto getBookById(@PathVariable Long id) {
