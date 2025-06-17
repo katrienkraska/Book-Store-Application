8d8d94124adc6976553d40dc0253f194d89be89e
Kate Kraska <kate2002.kk42@gmail.com>
2025-06-17 19:07:24 +0300

    Add GlobalExceptionHandler and DTO validation

--- src/main/java/org/example/exception/CustomGlobalExceptionHandler.java
+++ src/main/java/org/example/exception/CustomGlobalExceptionHandler.java
@@ -2,17 +2,14 @@ package org.example.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @ControllerAdvice
-public class CustomGlobalExceptionHandler extends ResponseEntityExceptionHandler {
+public class CustomGlobalExceptionHandler {
 
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<Object> handleEntityNotFoundException(EntityNotFoundException e) {
@@ -28,23 +25,14 @@ public class CustomGlobalExceptionHandler extends ResponseEntityExceptionHandler
         return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 
-    private String getErrorMessage(ObjectError e) {
-        if (e instanceof FieldError fieldError) {
-            String filed = fieldError.getField();
-            String massage = e.getDefaultMessage();
-            return filed + " " + massage;
-        }
-        return e.getDefaultMessage();
-    }
-
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleAllExceptions(Exception ex) {
+    public ResponseEntity<String> handleAllExceptions(Exception e) {
         return new ResponseEntity<>(
                 "An unexpected error occurred", HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(NullPointerException.class)
-    public ResponseEntity<String> handleNullPointerExceptions(NullPointerException ex) {
+    public ResponseEntity<String> handleNullPointerExceptions(NullPointerException e) {
         return new ResponseEntity<>(
                 "Null pointer exception occurred", HttpStatus.INTERNAL_SERVER_ERROR);
     }
