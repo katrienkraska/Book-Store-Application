708d8698ca5f52d427fbfa7d713380c107b0d01d
Kate Kraska <kate2002.kk42@gmail.com>
2025-06-24 21:47:41 +0300

    Fixed mentor code review comments.
    # Conflicts:
    #	.idea/workspace.xml

--- src/main/java/org/example/controller/BookController.java
+++ src/main/java/org/example/controller/BookController.java
@@ -1,11 +1,15 @@
 package org.example.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.example.dto.BookDto;
 import org.example.dto.CreateBookRequestDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.example.service.BookService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -18,6 +22,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Book API", description = "Book operations")
 @RestController
 @RequestMapping("/books")
 @RequiredArgsConstructor
@@ -25,30 +30,72 @@ public class BookController {
 
     private final BookService bookService;
 
+    @Operation(summary = "Get a list of all books with pagination",
+            description = "Fetches a paginated and sorted list of all available books. "
+                    + "Sorting can be applied by providing the sorting criteria "
+                    + "in the request parameters. "
+                    + "The sorting criteria should be specified in the format: "
+                    + "'sort: field,(ASC||DESC)' "
+                    + "where 'ASC' is ascending and 'DESC' is descending. "
+                    + "Default sorting is by in ascending order. "
+                    + "Only users with 'USER' authority can access this endpoint."
+    )
+    @PreAuthorize("hasAuthority('USER')")
     @GetMapping
     public Page<BookDto> findAll(Pageable pageable) {
         return bookService.findAll(pageable);
     }
 
+    @Operation(summary = "Get book by ID",
+            description = "Fetches a book from the system using its unique identifier. "
+                    + "Only users with 'USER' authority can access this endpoint.")
+    @PreAuthorize("hasAuthority('USER')")
     @GetMapping("/{id}")
-    public BookDto getBookById(@PathVariable Long id) {
+    public BookDto getBookById(
+            @Parameter(description = "ID book", required = true)
+            @PathVariable Long id) {
         return bookService.getBookById(id);
     }
 
+    @Operation(summary = "Create a new book",
+            description = "Adds a new book to the catalog. Only users with "
+                    + "'ADMIN' authority can perform this operation. "
+                    + "The request body must contain valid book details."
+    )
+    @PreAuthorize("hasAuthority('ADMIN')")
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public BookDto createBook(@RequestBody @Valid CreateBookRequestDto bookDto) {
+    public BookDto createBook(
+            @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "New book details")
+            @RequestBody @Valid CreateBookRequestDto bookDto) {
         return bookService.createBook(bookDto);
     }
 
+    @Operation(summary = "Update an existing book",
+            description = "Updates an existing book identified by the given ID. "
+                    + "Only users with 'ADMIN' authority can perform this operation. "
+                    + "The request body must contain valid book details."
+    )
+    @PreAuthorize("hasAuthority('ADMIN')")
     @PutMapping("/{id}")
-    public BookDto update(@PathVariable Long id, @RequestBody @Valid CreateBookRequestDto updateBook) {
+    public BookDto update(
+            @Parameter(description = "Book ID to update", required = true)
+            @PathVariable Long id,
+            @RequestBody @Valid CreateBookRequestDto updateBook) {
         return bookService.update(id, updateBook);
     }
 
+    @Operation(summary = "Delete book by ID",
+            description = "Deletes a book identified by the given ID. "
+                    + "Only users with 'ADMIN' authority can perform this operation."
+    )
+    @PreAuthorize("hasAuthority('ADMIN')")
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void delete(@PathVariable Long id) {
+    public void delete(
+            @Parameter(description = "Book ID to delete", required = true)
+            @PathVariable Long id) {
         bookService.delete(id);
     }
 }
