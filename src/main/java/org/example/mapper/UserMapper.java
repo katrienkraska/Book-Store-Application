0dcb200e3a5aa59a82a864b0351d4ec05793d7a4
Kate Kraska <kate2002.kk42@gmail.com>
2025-07-15 12:17:04 +0300

    ixed mentor code review comments.

--- src/main/java/org/example/mapper/UserMapper.java
+++ src/main/java/org/example/mapper/UserMapper.java
@@ -4,9 +4,11 @@ import org.example.dto.user.UserRegistrationRequestDto;
 import org.example.dto.user.UserResponseDto;
 import org.example.model.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
+    @Mapping(source = "firstName", target = "firstName")
     User toModel(UserRegistrationRequestDto requestDto);
 
     UserResponseDto toDto(User user);
