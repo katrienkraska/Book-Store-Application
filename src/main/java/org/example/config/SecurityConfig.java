316f9d79e83b3f727e0e6da286454532b7eab8c2
Kate Kraska <kate2002.kk42@gmail.com>
2025-07-16 19:14:21 +0300

    fixed mentor code review comments.

--- src/main/java/org/example/config/SecurityConfig.java
+++ src/main/java/org/example/config/SecurityConfig.java
@@ -3,9 +3,11 @@ package org.example.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -14,12 +16,19 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable()) // Вимикаємо CSRF для Postman
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/registration").permitAll() // Дозволяємо реєстрацію
-                        .anyRequest().authenticated() // Усі інші — з авторизацією
+                .cors(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(
+                        auth -> auth
+                                .requestMatchers(
+                                        antMatcher("/auth/**"),
+                                        antMatcher("/swagger-ui/**"),
+                                        antMatcher("/v3/api-docs/**"))
+                                .permitAll()
+                                .anyRequest()
+                                .authenticated()
                 )
-                .httpBasic(Customizer.withDefaults()); // Сучасна форма для httpBasic()
+                .httpBasic(Customizer.withDefaults());
 
         return http.build();
     }
