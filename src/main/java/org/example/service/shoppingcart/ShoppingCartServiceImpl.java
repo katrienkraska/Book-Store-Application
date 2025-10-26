247242f4bebe97b97826ad2a5853708eb186a761
Kate Kraska <kate2002.kk42@gmail.com>
2025-10-26 22:42:30 +0200

    Add Order model

--- src/main/java/org/example/service/shoppingcart/ShoppingCartServiceImpl.java
+++ src/main/java/org/example/service/shoppingcart/ShoppingCartServiceImpl.java
@@ -42,48 +42,38 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
 
     @Override
     public ShoppingCartDto save(Long authenticationId, CartItemRequestDto requestDto) {
-        // Знайти користувача
         User user = userRepository.findById(authenticationId)
                 .orElseThrow(() -> new EntityNotFoundException(
                         "Can't get user by Id " + authenticationId));
 
-        // Знайти книгу
         Book book = bookRepository.findById(requestDto.getBookId())
                 .orElseThrow(() -> new EntityNotFoundException(
                         "Can't find book by id " + requestDto.getBookId()));
 
-        // Отримати корзину користувача або створити нову
         ShoppingCart shoppingCart = shoppingCartRepository.findByUserId(user.getId())
                 .orElseGet(() -> {
                     ShoppingCart newCart = new ShoppingCart();
                     newCart.setUser(user);
-                    // Зберігаємо спочатку корзину, щоб Hibernate присвоїв ID
                     return shoppingCartRepository.saveAndFlush(newCart);
                 });
 
-        // Перевірити наявність CartItem
         CartItem cartItem = shoppingCart.getCartItems().stream()
                 .filter(item -> item.getBook().getId().equals(book.getId()))
                 .findFirst()
                 .orElse(null);
 
         if (cartItem != null) {
-            // Збільшуємо кількість
             cartItem.setQuantity(cartItem.getQuantity() + requestDto.getQuantity());
         } else {
-            // Створюємо новий CartItem
             cartItem = new CartItem();
             cartItem.setBook(book);
             cartItem.setQuantity(requestDto.getQuantity());
             cartItem.setShoppingCart(shoppingCart);
-            // Додаємо до корзини
             shoppingCart.getCartItems().add(cartItem);
         }
 
-        // Зберігаємо CartItem
         cartItemRepository.save(cartItem);
 
-        // Повертаємо актуальну корзину
         return cartItemMapper.toDto(shoppingCartRepository.findByUserId(user.getId())
                 .orElseThrow(() -> new EntityNotFoundException(
                         "Can't get user by Id " + authenticationId)));
